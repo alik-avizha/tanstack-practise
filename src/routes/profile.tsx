2a2a652ec1dc90e9b0cd25519d9692f82c1f3c01
alik-avizha <aleksandravizha@icloud.com>
2026-06-06 17:27:32 +0300

    feat(*): Block navigation with unsaved changes

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, redirect, useBlocker } from "@tanstack/react-router";
+import { type ChangeEvent, useState } from "react";
 
 export const Route = createFileRoute("/profile")({
   beforeLoad: ({ context }) => {
@@ -13,5 +14,28 @@ export const Route = createFileRoute("/profile")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/profile"!</div>;
+  const [name, setName] = useState("");
+  const { proceed, reset, status } = useBlocker({
+    condition: !!name,
+  });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <h1>Hello "/profile"!</h1>
+      <input type="text" value={name} onChange={onChange} />
+      {status === "blocked" && (
+        <div>
+          <div>You might have some unsaved changes in this page.</div>
+          <div>
+            <button onClick={reset}>Cancel</button>
+            <button onClick={proceed}>Continue</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
